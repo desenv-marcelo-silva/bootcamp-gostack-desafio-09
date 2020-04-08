db332c614d0811e5a1fae4c7ee9c0510c2d590cd
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-08 00:07:11 -0300

    Estilização do componente Header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, Content, Logo, Menu, Identificacao } from './styles';
 
 export default function Header() {
   return (
     <Container>
-      <h1>Header</h1>
+      <Content>
+        <Logo />
+        <nav>
+          <Menu>
+            <li>
+              <Link to="/deliveries">ENCOMENDAS</Link>
+            </li>
+            <li>
+              <Link to="/deliverymans">ENTREGADORES</Link>
+            </li>
+            <li>
+              <Link to="/recipients">DESTINATÁRIOS</Link>
+            </li>
+            <li>
+              <Link to="/problems">PROBLEMAS</Link>
+            </li>
+          </Menu>
+        </nav>
+      </Content>
+      <Identificacao>
+        <span>Admin FastFeet</span>
+        <Link to="/">Sair do Sistema</Link>
+      </Identificacao>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,69 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import logo from '../../assets/logo.png';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1440px;
+  margin: 0 auto;
+
+  background: #fff;
+  border: 1px solid #ddd;
+  padding: 0 20px;
+  height: 65px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Logo = styled.img.attrs({
+  src: logo,
+  alt: 'Logotipo FastFeet',
+})`
+  width: 135px;
+  height: 26px;
+  margin-right: 20px;
+  padding-right: 20px;
+  border-right: 1px solid #ddd;
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  align-items: center;
+
+  li {
+    margin-right: 5px;
+    padding-right: 10px;
+    font-weight: bold;
+
+    > a {
+      color: #999;
+
+      &:hover {
+        color: #000;
+      }
+    }
+  }
+`;
+
+export const Identificacao = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+
+  span {
+    color: #666;
+    margin-bottom: 10px;
+  }
+
+  > a {
+    font-size: 14px;
+    color: red;
+    text-transform: lowercase;
+  }
+`;
