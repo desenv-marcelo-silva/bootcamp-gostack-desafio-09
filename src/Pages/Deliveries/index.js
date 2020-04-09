57e0cc62403d184eebe319a1ef0511f6459cbd2b
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-08 23:35:22 -0300

    Página de entregas.

--- src/Pages/Deliveries/index.js
+++ src/Pages/Deliveries/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { MdSearch, MdAdd } from 'react-icons/md';
 
-import { Container } from './styles';
+import {
+  Container,
+  TopoForm,
+  DeliveryTable,
+  DeliveryCab,
+  DeliveryRow,
+  DeliveryColumn,
+} from './styles';
 import TitleActiveWork from '../../components/TitleActiveWork';
 
 export default function Deliveries() {
   return (
     <Container>
       <TitleActiveWork title="Gerenciando encomendas" />
+      <TopoForm>
+        <Form onSubmit={() => {}}>
+          <div>
+            <span>
+              <MdSearch />
+            </span>
+            <Input
+              type="text"
+              name="filtro"
+              placeholder="Busca por encomendas"
+            />
+          </div>
+        </Form>
+
+        <button type="button">
+          <MdAdd />
+          <span>CADASTRAR</span>
+        </button>
+      </TopoForm>
+
+      <DeliveryTable>
+        <DeliveryCab>
+          <DeliveryColumn>ID</DeliveryColumn>
+          <DeliveryColumn>Destinatário</DeliveryColumn>
+          <DeliveryColumn>Entregador</DeliveryColumn>
+          <DeliveryColumn>Cidade</DeliveryColumn>
+          <DeliveryColumn>Estado</DeliveryColumn>
+          <DeliveryColumn>Status</DeliveryColumn>
+          <DeliveryColumn>Ações</DeliveryColumn>
+        </DeliveryCab>
+        <DeliveryRow>
+          <DeliveryColumn>#01</DeliveryColumn>
+          <DeliveryColumn>Ludwig van Beethoven</DeliveryColumn>
+          <DeliveryColumn>JD jonh Doe</DeliveryColumn>
+          <DeliveryColumn>Rio do Sul</DeliveryColumn>
+          <DeliveryColumn>Santa Catarina</DeliveryColumn>
+          <DeliveryColumn>* ENTREGUE</DeliveryColumn>
+          <DeliveryColumn>...</DeliveryColumn>
+        </DeliveryRow>
+      </DeliveryTable>
     </Container>
   );
 }
--- src/Pages/Deliveries/styles.js
+++ src/Pages/Deliveries/styles.js
@@ -1,3 +1,90 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export const TopoForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 25px;
+
+  form {
+    div {
+      display: flex;
+      align-items: center;
+
+      background: #fff;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      width: 240px;
+      padding: 4px;
+
+      span {
+        margin-left: 5px;
+        margin-right: 10px;
+
+        color: #999;
+      }
+
+      svg {
+        height: 18px;
+        width: 18px;
+      }
+
+      input {
+        font-size: 14px;
+        height: 20px;
+
+        border: 0;
+
+        &::placeholder {
+          color: #999;
+        }
+      }
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    padding-right: 5px;
+    border: 0;
+
+    background: #7d40e7;
+    border-radius: 4px;
+
+    width: 140px;
+    height: 36px;
+
+    color: #fff;
+    font-weight: bold;
+    font-size: 14px;
+
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
+`;
+
+export const DeliveryTable = styled.div`
+  display: table;
+  margin-top: 20px;
+`;
+
+export const DeliveryCab = styled.div`
+  display: table-row;
+  font-weight: bold;
+`;
+
+export const DeliveryRow = styled.div`
+  display: table-row;
+`;
+
+export const DeliveryColumn = styled.div`
+  display: table-cell;
+`;
