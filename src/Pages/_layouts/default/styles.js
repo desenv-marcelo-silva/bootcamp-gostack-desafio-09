baf06bf7ad19f501f3dc361130f99e89b627ec50
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-10 17:15:25 -0300

    Mudança de cor para ficar mais visível o layout.

--- src/Pages/_layouts/default/styles.js
+++ src/Pages/_layouts/default/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100vh;
-  background: #f5f5f5;
+  /* background: #f5f5f5; */
+  background: #b9b9b9;
 
   max-width: 1400px;
   margin: 0 auto;
