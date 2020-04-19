160cd55fdc3e929c8211f796354a5822b0f5f12f
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-19 12:29:11 -0300

    babel-plugin-root: instalação e configuração.

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const { addBabelPlugin, override } = require('cutomize-cra');
+
+module.exports = override(
+  addBabelPlugin([
+    'babel-plugin-root-import',
+    {
+      rootPathSuffix: 'src',
+    },
+  ])
+);
