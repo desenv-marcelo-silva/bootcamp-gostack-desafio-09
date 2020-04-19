ccc9dd4c0ae96181cab53a105018cb09fd533dca
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-04-19 13:46:24 -0300

    Unform: Instalação e uso no sigin.

--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
+  function handleSubmit({ email, password }) {}
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
-        <label htmlFor="email">seu e-mail</label>
-        <input id="email" type="email" placeholder="digite seu e-mail" />
-        <label htmlFor="password">Sua senha</label>
-        <input id="password" type="password" placeholder="digite sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <label>seu e-mail</label>
+        <Input name="email" type="email" placeholder="digite seu e-mail" />
+        <label>Sua senha</label>
+        <Input name="password" type="password" placeholder="digite sua senha" />
 
         <button type="submit">Acessar</button>
-      </form>
+      </Form>
     </>
   );
 }
