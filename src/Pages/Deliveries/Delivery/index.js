68d0112094cebd23ac44c25299e5383f1ca5b810
MacBook Pro <macbookpro@MacBook-Pro-de-Marcelo.local>
2020-05-01 23:23:59 -0300

    feat: cadastro de encomendas

--- src/Pages/Deliveries/Delivery/index.js
+++ src/Pages/Deliveries/Delivery/index.js
@@ -1,15 +1,101 @@
 import React from 'react';
+import { MdChevronLeft, MdCheck } from 'react-icons/md';
+import { Form, Input, Select } from '@rocketseat/unform';
+
 import history from '~/services/history';
 
-import { Container } from './styles';
+import { Container, Topo, FormArea } from './styles';
+
+const mockRecipients = [
+  { id: 1, title: 'Julio Jones' },
+  { id: 2, title: 'Davante Adams' },
+  { id: 3, title: 'Odell Beckham' },
+  { id: 4, title: 'DeAndre Hopkins' },
+  { id: 5, title: 'Michael Thomas' },
+  { id: 6, title: 'JuJu Smith-Schuster' },
+  { id: 7, title: 'Tyreek Hill' },
+  { id: 8, title: 'Mike Evans' },
+  { id: 9, title: 'Keenan Allen' },
+  { id: 10, title: 'Amari Cooper' },
+];
+
+const mockDeliveryman = [
+  { id: 1, title: 'Lamar Jackson' },
+  { id: 2, title: 'Patrick Mahomes' },
+  { id: 3, title: 'Drew Brees' },
+  { id: 4, title: 'Dak Prescott' },
+  { id: 5, title: 'Russell Wilson' },
+  { id: 6, title: 'Matthew Stafford' },
+  { id: 7, title: 'Deshaun Watson' },
+  { id: 8, title: 'Ryan Fitzpatrick' },
+  { id: 9, title: 'Derek Carr' },
+  { id: 10, title: 'Carson Wentz' },
+];
 
 export default function Delivery() {
+  function handleSubmit(params) {
+    console.tron.log(params);
+  }
+
   return (
     <Container>
-      <h1>Entregas</h1>
-      <button type="button" onClick={() => history.goBack()}>
-        Voltar
-      </button>
+      <FormArea>
+        <Form onSubmit={handleSubmit}>
+          <Topo>
+            <h2>Cadastro de encomendas</h2>
+            <div>
+              <button type="button" onClick={() => history.goBack()}>
+                <MdChevronLeft size={20} />
+                <span>Voltar</span>
+              </button>
+              <button type="submit">
+                <MdCheck size={20} />
+                <span>Salvar</span>
+              </button>
+            </div>
+          </Topo>
+          <div className="form-input-area">
+            <div className="form-input-area-line-1">
+              <label htmlFor="recipient">
+                Destinatário
+                <Select
+                  id="recipient"
+                  name="recipient"
+                  options={mockRecipients}
+                />
+                {/* {mockRecipients &&
+                    mockRecipients.map((opt) => (
+                      <option key={opt.value} value={opt.value}>
+                        {opt.label}
+                      </option>
+                    ))}
+                </Select> */}
+              </label>
+              <label htmlFor="deliveryman">
+                Entregador
+                <Select
+                  id="deliveryman"
+                  name="deliveryman"
+                  options={mockDeliveryman}
+                />
+                {/* {mockDeliveryman &&
+                    mockDeliveryman.map((opt) => (
+                      <option key={opt.value} value={opt.value}>
+                        {opt.label}
+                      </option>
+                    ))}
+                </Select> */}
+              </label>
+            </div>
+            <div className="form-input-area-line-2">
+              <label htmlFor="product">
+                Nome do produto
+                <Input type="text" id="product" name="product" />
+              </label>
+            </div>
+          </div>
+        </Form>
+      </FormArea>
     </Container>
   );
 }
--- src/Pages/Deliveries/Delivery/styles.js
+++ src/Pages/Deliveries/Delivery/styles.js
@@ -1,3 +1,119 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 900px;
+  height: 300px;
+  margin: 0 auto;
+`;
+
+export const Topo = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+
+  h2 {
+    color: #666;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    width: 190px;
+
+    button {
+      display: flex;
+      justify-content: space-around;
+
+      width: 90px;
+      text-transform: uppercase;
+      font-size: 14px;
+
+      color: #fff;
+      background: #7159c1;
+      border-radius: 4px;
+
+      &:first-child {
+        background: #c3c3c3;
+      }
+
+      span {
+        margin-right: 5px;
+      }
+
+      > svg {
+        margin-left: 5px;
+      }
+
+      &:hover {
+        background: ${darken(0.05, '#7159c1')};
+      }
+
+      &:first-child:hover {
+        background: ${darken(0.05, '#c3c3c3')};
+      }
+    }
+  }
+`;
+
+export const FormArea = styled.div`
+  width: 100%;
+  height: 90%;
+  background: transparent;
+  padding: 20px;
+  border-radius: 4px;
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    div.form-input-area {
+      background: #fff;
+      padding: 30px 0 30px 30px;
+    }
+
+    label {
+      color: #444;
+      font-weight: 700;
+      font-size: 14px;
+    }
+
+    div.form-input-area-line-1 {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+
+      label {
+        width: 50%;
+
+        > select {
+          width: 90%;
+          height: 40px;
+          font-size: 16px;
+          color: #444;
+          background: transparent;
+        }
+      }
+    }
+
+    div.form-input-area-line-2 {
+      width: 100%;
+      margin-top: 10px;
+
+      label {
+        width: 100%;
+
+        input {
+          margin-top: 5px;
+          padding-left: 5px;
+          width: 95%;
+          height: 40px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          color: #444;
+        }
+      }
+    }
+  }
+`;
